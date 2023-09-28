8caeb0ba68dd30fb4d4a3c9ea6f213bccf2425c2
masouddaloji <masouddaloji@gmail.com>
2023-09-28 19:35:32 +0330

    fixed rename root id

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/index.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("paev_sell_ticket_react")!).render(
   <React.StrictMode>
     <main className="mx-auto mt-8 max-w-3xl !bg-white p-4 md:p-8 sell-ticket-container">
       <RouterProvider router={router} fallbackElement={<div>Loading...</div>} />
